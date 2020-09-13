4eab465e6143d880aaa19c24a9e946555c71e191
Алексей <guha5277@yandex.ru>
2020-09-13 17:42:46 +0700

    Добавление полей класса

--- src/ru/guhar4k/repository/RegionRepositoryImpl.java
+++ src/ru/guhar4k/repository/RegionRepositoryImpl.java
@@ -2,7 +2,12 @@ package ru.guhar4k.repository;
 
 import ru.guhar4k.model.Region;
 
+import java.io.File;
+
 public class RegionRepositoryImpl implements RegionRepository {
+    private final String FILE_PATH = "regions.txt";
+    private final File repoFile = new File(FILE_PATH);
+
     @Override
     public void save(Region obj) {
 
