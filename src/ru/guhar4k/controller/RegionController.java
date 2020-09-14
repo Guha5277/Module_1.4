c865a22b36a53b2ee86fc21d4020d42bf0f77fbd
Guha5277 <guha5277@yandex.ru>
2020-09-14 13:16:19 +0700

    Реализация RegionController

--- src/ru/guhar4k/controller/RegionController.java
+++ src/ru/guhar4k/controller/RegionController.java
@@ -1,4 +1,35 @@
 package ru.guhar4k.controller;
 
+import ru.guhar4k.model.Region;
+import ru.guhar4k.repository.RegionRepository;
+import ru.guhar4k.repository.RegionRepositoryImpl;
+
+import java.util.List;
+
 public class RegionController {
+    private RegionRepository regionRepository;
+
+    public RegionController() {
+        regionRepository = new RegionRepositoryImpl();
+    }
+
+    public void save(Region region) {
+        regionRepository.save(region);
+    }
+
+    public Region getById(Long id) {
+        return regionRepository.getById(id);
+    }
+
+    public void update(Region region) {
+        regionRepository.update(region);
+    }
+
+    public void deleteById(Long id) {
+        regionRepository.deleteById(id);
+    }
+
+    public List<Region> getAll() {
+        return regionRepository.getAll();
+    }
 }
