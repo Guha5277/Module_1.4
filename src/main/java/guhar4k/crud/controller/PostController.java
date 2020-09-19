9c8eadb918f843cf486330b4b04c1eb80a328b9d
Guha5277 <guha5277@yandex.ru>
2020-09-19 11:18:36 +0700

    Реализация PostController

--- src/main/java/guhar4k/crud/controller/PostController.java
+++ src/main/java/guhar4k/crud/controller/PostController.java
@@ -1,4 +1,35 @@
 package guhar4k.crud.controller;
 
+import guhar4k.crud.model.Post;
+import guhar4k.crud.repository.PostRepository;
+import guhar4k.crud.repository.PostRepositoryImpl;
+
+import java.util.List;
+
 public class PostController{
+    private PostRepository postRepository;
+
+    public PostController() {
+        this.postRepository = new PostRepositoryImpl();
+    }
+
+    public void save(Post post){
+        postRepository.save(post);
+    }
+
+    public Post getById(Long id){
+        return postRepository.getById(id);
+    }
+
+    public void update(Post post){
+        postRepository.update(post);
+    }
+
+    public void deleteById(Long id){
+        postRepository.deleteById(id);
+    }
+
+    public List<Post> getAll(){
+        return postRepository.getAll();
+    }
 }
