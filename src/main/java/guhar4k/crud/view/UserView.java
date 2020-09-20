35d6836ee0415bb5323dd47bc4124c2e836f92b4
Guha5277 <guha5277@yandex.ru>
2020-09-20 07:51:52 +0700

    Добавление конструктора и полей класса

--- src/main/java/guhar4k/crud/view/UserView.java
+++ src/main/java/guhar4k/crud/view/UserView.java
@@ -1,4 +1,15 @@
 package guhar4k.crud.view;
 
-public class UserView {
+import guhar4k.crud.controller.UserController;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class UserView extends View{
+    private UserController userController;
+
+    UserView() {
+        super(new Scanner(System.in), new PrintStream(System.out), "База данных пользователей");
+        userController = new UserController();
+    }
 }
