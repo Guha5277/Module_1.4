262c4052b21acbb2dc6926c64ae5db903a166dc3
Алексей <guha5277@yandex.ru>
2020-09-20 19:54:09 +0700

    Переопределение и тестовая реализация метода editRecord. Не все комманды выполняются корректно!

--- src/main/java/guhar4k/crud/view/UserView.java
+++ src/main/java/guhar4k/crud/view/UserView.java
@@ -1,15 +1,17 @@
 package guhar4k.crud.view;
 
 import guhar4k.crud.controller.UserController;
+import guhar4k.crud.model.Post;
 import guhar4k.crud.model.Region;
 import guhar4k.crud.model.User;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class UserView extends View{
+public class UserView extends View {
     private final String CMD_USER_FNAME = "fname";
     private final String CMD_USER_LNAME = "lname";
     private final String CMD_USER_REGION = "region";
@@ -31,10 +33,10 @@ public class UserView extends View{
                 CMD_GET_ALL + " - получение всех пользователей\n\t" +
                 CMD_GET_BY_ID + " <ID> - получение одного пользователя по ID\n\t" +
                 CMD_EDIT_BY_ID + " - редактирование существующего пользователя, осуществляется по следующему шаблону:\n\t\t" +
-                CMD_EDIT_BY_ID + " " + CMD_USER_FNAME + " <FIRST_NAME> - изменение имени пользователя\n\t\t" +
-                CMD_EDIT_BY_ID + " " + CMD_USER_LNAME + " <LAST_NAME> - изменение фамилии пользователя\n\t\t" +
-                CMD_EDIT_BY_ID + " " + CMD_USER_REGION + " <REGION_ID> - изменение страны пользователя(необходимо ввести ID страны)\n\t\t" +
-                CMD_EDIT_BY_ID + " " + CMD_USER_POSTS + " <POST_ID1 POST_ID2...> - изменение списка постов пользователя (вводятся ID постов разделенные пробелом)\n\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_FNAME + " <ID> <FIRST_NAME> - изменение имени пользователя\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_LNAME + " <ID> <LAST_NAME> - изменение фамилии пользователя\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_REGION + " <ID> <REGION_ID> - изменение страны пользователя(необходимо ввести ID страны)\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_POSTS + " <ID> <POST_ID1 POST_ID2...> - изменение списка постов пользователя (вводятся ID постов разделенные пробелом)\n\t" +
                 CMD_EXIT + " - выход из программы");
     }
 
@@ -62,7 +64,7 @@ public class UserView extends View{
     void showAllRecords() {
         showMsg("Получение всех записей...");
         List<User> users = userController.getAll();
-        if (users.size() == 0){
+        if (users.size() == 0) {
             showError("Список пользователей пуст");
             return;
         }
@@ -104,5 +106,70 @@ public class UserView extends View{
         } else {
             showError("Отсутствуют аргументы для комманды " + CMD_GET_BY_ID);
         }
+    }
+
+    @Override
+    void editRecord(String[] command) {
+        for (String s : command){
+            showMsg(s);
+        }
+        if (command.length < 4) {
+            showError("Неверное количество аргументов для сомманды: " + CMD_EDIT_BY_ID);
+            return;
+        }
+
+        long id;
+        try {
+            int ID_PART = 2;
+            id = Long.valueOf(command[ID_PART]);
+        } catch (NumberFormatException e) {
+            showError("Неверный формат комманды (неверно указан id редактируемого пользователя)!");
+            return;
+        }
+
+        int SUB_COMMAND = 1;
+        int FIRST_ARGUMENT = 3;
+        User user;
+        try {
+            user = userController.getById(id);
+        } catch (NoSuchElementException e) {
+            showError("Отсутствует пользователь с заданным ID");
+            return;
+        }
+
+        switch (command[SUB_COMMAND]) {
+            case CMD_USER_FNAME:
+                user.setFirstName(command[FIRST_ARGUMENT]);
+                userController.update(user);
+                break;
+            case CMD_USER_LNAME:
+                user.setLastName(command[FIRST_ARGUMENT]);
+                userController.update(user);
+                break;
+            case CMD_USER_REGION:
+                try {
+                    user.setRegion(new Region(Long.valueOf(command[FIRST_ARGUMENT])));
+                    userController.update(user);
+                } catch (NumberFormatException e) {
+                    showError("Неверный формат команды (неверно указан id регоина");
+                }
+                break;
+            case CMD_USER_POSTS:
+                List<Post> posts = new ArrayList<>();
+                try {
+                for (int i = 3; i < command.length ; i++) {
+                    posts.add(new Post(Long.valueOf(command[i])));
+                }
+                } catch (NumberFormatException e){
+                    showError("Неверный формат комманды (неверно указаны id постов)");
+                    return;
+                }
+                user.setPosts(posts);
+                userController.update(user);
+                break;
+            default:
+                showError("Неизвестная суб-комманда комманды " + CMD_EDIT_BY_ID);
+        }
+        showAllRecords();
     }
 }
