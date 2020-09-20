fdb5e8c196f8779e9efff0bbcfdd4762b00ae022
Алексей <guha5277@yandex.ru>
2020-09-20 11:42:25 +0700

    Добавление полей-констант в качестве дополнительных команд-констант. Переопределение метода справки - showHelp

--- src/main/java/guhar4k/crud/view/UserView.java
+++ src/main/java/guhar4k/crud/view/UserView.java
@@ -6,9 +6,13 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class UserView extends View{
+    private final String CMD_USER_FNAME = "fname";
+    private final String CMD_USER_LNAME = "lname";
+    private final String CMD_USER_REGION = "region";
+    private final String CMD_USER_POSTS = "posts";
     private UserController userController;
 
-    UserView() {
+    public UserView() {
         super(new Scanner(System.in), new PrintStream(System.out), "База данных пользователей");
         userController = new UserController();
         start();
@@ -18,11 +22,15 @@ public class UserView extends View{
     void showHelp() {
         showMsg("Записи выводятся в формате <ID>|<FNAME>|<LNAME>|<POSTS_ID>|<REGION>\n\t" +
                 CMD_HELP + " - вывод справки\n\t" +
-                CMD_CREATE + " <FNAME> <LNAME> <REGION> <POSTS_ID> - создание новой записи, POSTS_ID указывается через запятую\n\t" +
-                CMD_DELETE + " <ID> - удаление записи\n\t" +
-                CMD_GET_ALL + " - получение всех записей\n\t" +
-                CMD_GET_BY_ID + " <ID> - получение одной записи по ID\n\t" +
-                CMD_EDIT_BY_ID + " <ID> <FNAME> <LNAME> <REGION> <POSTS_ID> - изменение существующей записи\n\t" +
+                CMD_CREATE + " <FIRST_NAME> <LAST_NAME> <REGION> - создание нового пользователя\n\t" +
+                CMD_DELETE + " <ID> - удаление пользователя\n\t" +
+                CMD_GET_ALL + " - получение всех пользователей\n\t" +
+                CMD_GET_BY_ID + " <ID> - получение одного пользователя по ID\n\t" +
+                CMD_EDIT_BY_ID + " - редактирование существующего пользователя, осуществляется по следующему шаблону:\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_FNAME + " <FIRST_NAME> - изменение имени пользователя\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_LNAME + " <LAST_NAME> - изменение фамилии пользователя\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_REGION + " <REGION_ID> - изменение страны пользователя(необходимо ввести ID страны)\n\t\t" +
+                CMD_EDIT_BY_ID + " " + CMD_USER_POSTS + " <POST_ID1 POST_ID2...> - изменение списка постов пользователя (вводятся ID постов разделенные пробелом)\n\t" +
                 CMD_EXIT + " - выход из программы");
     }
 }
