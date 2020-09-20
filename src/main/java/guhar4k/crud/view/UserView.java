ccae02c79646933232495126a047b352af64d1d7
Guha5277 <guha5277@yandex.ru>
2020-09-20 08:24:39 +0700

    Переопределение метода showHelp

--- src/main/java/guhar4k/crud/view/UserView.java
+++ src/main/java/guhar4k/crud/view/UserView.java
@@ -11,5 +11,18 @@ public class UserView extends View{
     UserView() {
         super(new Scanner(System.in), new PrintStream(System.out), "База данных пользователей");
         userController = new UserController();
+        start();
+    }
+
+    @Override
+    void showHelp() {
+        showMsg("Записи выводятся в формате <ID>|<FNAME>|<LNAME>|<POSTS_ID>|<REGION>\n\t" +
+                CMD_HELP + " - вывод справки\n\t" +
+                CMD_CREATE + " <FNAME> <LNAME> <REGION> <POSTS_ID> - создание новой записи, POSTS_ID указывается через запятую\n\t" +
+                CMD_DELETE + " <ID> - удаление записи\n\t" +
+                CMD_GET_ALL + " - получение всех записей\n\t" +
+                CMD_GET_BY_ID + " <ID> - получение одной записи по ID\n\t" +
+                CMD_EDIT_BY_ID + " <ID> <FNAME> <LNAME> <REGION> <POSTS_ID> - изменение существующей записи\n\t" +
+                CMD_EXIT + " - выход из программы");
     }
 }
