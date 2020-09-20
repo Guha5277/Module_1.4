7afe0901e31e44e09250abe9ecd77bae994b0389
Guha5277 <guha5277@yandex.ru>
2020-09-20 07:47:48 +0700

    Добавление нового разделителя для постов пользователя

--- src/main/java/guhar4k/crud/utils/Library.java
+++ src/main/java/guhar4k/crud/utils/Library.java
@@ -3,4 +3,5 @@ package guhar4k.crud.utils;
 public class Library {
     public static final String REC_END = "<!e>";
     public static final String REC_PART_DELIMITER = "<!p>";
+    public static final String REC_POST_ID_DELIMITER = ",";
 }
