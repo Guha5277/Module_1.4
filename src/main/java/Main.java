7791a67099063817e5db6a128a6cdfad6b8390e5
Guha5277 <guha5277@yandex.ru>
2020-09-21 08:06:07 +0700

    Добавление файлов репозитория и Main-класса

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import guhar4k.crud.view.PostView;
+import guhar4k.crud.view.RegionView;
+import guhar4k.crud.view.UserView;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            switch (args[1]){
+                case "region":
+                    new RegionView();
+                    break;
+                case "user":
+                    new UserView();
+                    break;
+                case "post":
+                    new PostView();
+                    break;
+                default:
+                    System.out.println("Незивестный аргумент. Завершение программы...");
+            }
+        } else {
+            System.out.println("Запустите приложение с одним из аргументов, для продолжения работы!");
+        }
+    }
+}
