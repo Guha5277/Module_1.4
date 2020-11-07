d5de2c8671919089d9b21f84cd784bc09e575180
Алексей <guha5277@yandex.ru>
2020-11-07 19:55:35 +0700

    Частичная реализация класса работы с репозиторием в JSON

--- src/main/java/guhar4k/crud/repository/json/JsonRegionRepositoryImpl.java
+++ src/main/java/guhar4k/crud/repository/json/JsonRegionRepositoryImpl.java
@@ -1,14 +1,29 @@
 package guhar4k.crud.repository.json;
 
+import com.google.gson.reflect.TypeToken;
 import guhar4k.crud.model.Region;
 import guhar4k.crud.repository.RegionRepository;
+import guhar4k.crud.utils.JsonUtils;
+import guhar4k.crud.utils.Library;
+import guhar4k.crud.utils.Utils;
 
+import java.io.File;
 import java.util.List;
 
 public class JsonRegionRepositoryImpl implements RegionRepository {
+    private final String FILE_NAME = "posts.json";
+    private Utils<Region> ioUtils;
+
+    public JsonRegionRepositoryImpl() {
+        ioUtils = new JsonUtils<>(new File(Library.RES_DIR, FILE_NAME), new TypeToken<List<Region>>(){}.getType());
+        ioUtils.initRepositoryFile();
+    }
+
     @Override
     public void save(Region region) {
-
+        List<Region> regionList = getAll();
+        //region.setId(regionList.size() == 0 ? 1 : regionList.size() + 1);
+        ioUtils.saveRecord(region);
     }
 
     @Override
@@ -28,6 +43,6 @@ public class JsonRegionRepositoryImpl implements RegionRepository {
 
     @Override
     public List<Region> getAll() {
-        return null;
+        return ioUtils.getAll();
     }
 }
